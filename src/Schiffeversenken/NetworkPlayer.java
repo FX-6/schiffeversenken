239fc6627ae9bc2e7df3c80baa2c44397689dc43
FX-6 <felixschwinger@web.de>
2022-11-08 08:25:50 +0100

    Networking zu Constructor hinzugefügt

--- src/Schiffeversenken/NetworkPlayer.java
+++ src/Schiffeversenken/NetworkPlayer.java
@@ -1,17 +1,48 @@
 package Schiffeversenken;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class NetworkPlayer extends Player {
 
 	public final boolean isServer;
-	
+   public final int portNumber = 1337;
+   private Socket socket;
+   private PrintWriter out;
+   private BufferedReader in;
+
 	public NetworkPlayer(Game game, Player otherPlayer) {
 		super(game, otherPlayer);
+      try (ServerSocket serverSocket = new ServerSocket(this.portNumber)) {
+         this.socket = serverSocket.accept();
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
 		this.isServer = true;
+      try {
+         this.out = new PrintWriter(socket.getOutputStream(), true);
+         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
 	}
-	
+
 	public NetworkPlayer(Game game, Player otherPlayer, String ipAddress) {
 		super(game, otherPlayer);
 		this.isServer = false;
+      try {
+         this.socket = new Socket(ipAddress, this.portNumber);
+         this.out = new PrintWriter(socket.getOutputStream(), true);
+         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
 	}
 
 	// Anpassung an die Netzwerkstruktur
@@ -25,41 +56,41 @@ public class NetworkPlayer extends Player {
 	public void pass() {
 		// Teile über das Netzwerk mit, dass anderer Spieler dran ist und warte auf Antwort
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	public void sendReady() {
 		// Wenn Schiffsanzahl oder Feldgröße empfangen und gespeichert wurden
 	}
-	
+
 	public void sendOK() {
 		// Wenn Spiel Speichern oder Laden erfolgreich war
 	}
-	
+
 	public void sendExit() {
 		// Wenn spiel Laden nicht erfolgreich war oder Spiel verlassen werden soll
 	}
-	
-	
-	
+
+
+
 	// Netzwerkspezifisch als Server
 	public boolean sendSize(int pitchSize) {
 		// true, falls "done" als Antwork kam, andernfalls false
 		return false;
 	}
-	
+
 	public boolean sendShips(int[] ships) {
 		// true, falls "done" als Antwork kam, andernfalls false
 		return false;
 	}
-	
+
 	public boolean sendSave(String id) {
 		// true, falls "ok" als Antwork kam, andernfalls false
 		return false;
 	}
-	
+
 	public boolean sendLoad(String id) {
 		// true, falls "ok" als Antwort kam, andernfalls false
 		return false;
