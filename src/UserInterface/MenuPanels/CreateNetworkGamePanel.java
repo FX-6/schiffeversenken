0940577cff79da7e4c2333b7f2c0960060c4b4ab
FX-6 <felixschwinger@web.de>
2022-11-25 12:57:28 +0100

    UI Komponenten in Panels eingefügt

--- src/UserInterface/MenuPanels/CreateNetworkGamePanel.java
+++ src/UserInterface/MenuPanels/CreateNetworkGamePanel.java
@@ -1,6 +1,5 @@
 package UserInterface.MenuPanels;
 
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,6 +15,12 @@ import Notifications.NotificationCenter;
 import Schiffeversenken.GameType;
 import Schiffeversenken.Main;
 import UserInterface.Menu;
+import UserInterface.UIComponents.DualRowPanel;
+import UserInterface.UIComponents.InputButton;
+import UserInterface.UIComponents.InputPanel;
+import UserInterface.UIComponents.InputSpinner;
+import UserInterface.UIComponents.MenuButton;
+import UserInterface.UIComponents.WrapperPanel;
 
 /*
  * TODO: 	Keine falschen Eingaben in den Textfeldern;
@@ -26,112 +31,105 @@ import UserInterface.Menu;
 public class CreateNetworkGamePanel extends JPanel implements Notification {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -4350076887571572645L;
 
 	private Menu parent;
-	
-	private JButton start;
-	
+
+   private JButton startGameButton;
+
 	public CreateNetworkGamePanel(Menu parent, GameType type) {
 		this.parent = parent;
 		setup();
 		fillWithContent();
-		
+
 		parent.createGame(4, type, null);
 	}
-	
+
 	private void setup() {
 		NotificationCenter.addObserver("ClientConnected", this);
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 	}
-	
-	private void fillWithContent() {		
-		JButton menu = new MenuButton();
-		menu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				Main.currentGame.exit();
-			}
-		});
-		
-		SpinnerNumberModel spinner = new SpinnerNumberModel(5, 5, 25, 1);
-		JSpinner pitchSize = new JSpinner(spinner);
-		pitchSize.setMaximumSize(new Dimension(parent.getMaximumSize().width, 30));
-		pitchSize.setAlignmentX(LEFT_ALIGNMENT);
-		
-		
-		JPanel horizontal = new JPanel();
-		horizontal.setLayout(new BoxLayout(horizontal, BoxLayout.LINE_AXIS));
-		horizontal.setMaximumSize(new Dimension(parent.getMaximumSize().width, 30));
-		
-		JSpinner size2 = new JSpinner();
-		JSpinner size3 = new JSpinner();
-		JSpinner size4 = new JSpinner();
-		JSpinner size5 = new JSpinner();
-		
-		horizontal.add(Box.createHorizontalStrut(10));
-		horizontal.add(size2);
-		horizontal.add(Box.createGlue());
-		horizontal.add(size3);
-		horizontal.add(Box.createGlue());
-		horizontal.add(size4);
-		horizontal.add(Box.createGlue());
-		horizontal.add(size5);
-		horizontal.add(Box.createHorizontalStrut(10));
-		horizontal.setAlignmentX(LEFT_ALIGNMENT);
-		
-		
-		start = new JButton("Spiel starten");
-		start.setEnabled(false);
-		start.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				Main.currentGame.setPitchSize(Integer.parseInt(pitchSize.getValue().toString()));
-				
-				int[] ships = new int[4];
-				ships[0] = Integer.parseInt(size2.getValue().toString());
-				ships[1] = Integer.parseInt(size3.getValue().toString());
-				ships[2] = Integer.parseInt(size4.getValue().toString());
-				ships[3] = Integer.parseInt(size5.getValue().toString());
-				Main.currentGame.setShips(ships);
-				Main.currentGame.transmittSizeAndShips();
-				
-				parent.openGameWindow();
-			}
-		});
-		
-		add(menu);
-		add(Box.createGlue());
-		add(pitchSize);
-		add(Box.createGlue());
-		add(horizontal);
-		add(Box.createGlue());
-		add(start);
-		add(Box.createVerticalStrut(10));
+
+	private void fillWithContent() {
+      JPanel wrapperPanel = new WrapperPanel();
+
+      // Field size input and auto ship button
+      JPanel sizeRow = new DualRowPanel();
+      InputPanel sizeInputPanel = new InputPanel("Spielfeldgröße");
+      JSpinner sizeInput = new InputSpinner(new SpinnerNumberModel(5, 5, 25, 1));
+      sizeInputPanel.add(sizeInput);
+      JButton autoShipButton = new InputButton("Bevölkern");
+      sizeRow.add(sizeInputPanel);
+      sizeRow.add(autoShipButton);
+
+      // Ship size inputs
+      JPanel shipsInputRow1 = new DualRowPanel();
+      InputPanel ship2InputPanel = new InputPanel("2er Schiffe");
+      JSpinner ship2Input = new InputSpinner(new SpinnerNumberModel(0, 0, 10, 1));
+      ship2InputPanel.add(ship2Input);
+      InputPanel ship3InputPanel = new InputPanel("3er Schiffe");
+      JSpinner ship3Input = new InputSpinner(new SpinnerNumberModel(0, 0, 10, 1));
+      ship3InputPanel.add(ship3Input);
+      shipsInputRow1.add(ship2InputPanel);
+      shipsInputRow1.add(ship3InputPanel);
+
+      JPanel shipsInputRow2 = new DualRowPanel();
+      InputPanel ship4InputPanel = new InputPanel("4er Schiffe");
+      JSpinner ship4Input = new InputSpinner(new SpinnerNumberModel(0, 0, 10, 1));
+      ship4InputPanel.add(ship4Input);
+      InputPanel ship5InputPanel = new InputPanel("5er Schiffe");
+      JSpinner ship5Input = new InputSpinner(new SpinnerNumberModel(0, 0, 10, 1));
+      ship5InputPanel.add(ship5Input);
+      shipsInputRow2.add(ship4InputPanel);
+      shipsInputRow2.add(ship5InputPanel);
+
+      // Start game button
+      startGameButton = new InputButton("Spiel starten");
+      startGameButton.setEnabled(false);
+      startGameButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            Main.currentGame.setPitchSize(Integer.parseInt(sizeInput.getValue().toString()));
+
+            int[] ships = new int[4];
+            ships[0] = Integer.parseInt(ship2Input.getValue().toString());
+            ships[1] = Integer.parseInt(ship3Input.getValue().toString());
+            ships[2] = Integer.parseInt(ship4Input.getValue().toString());
+            ships[3] = Integer.parseInt(ship5Input.getValue().toString());
+            Main.currentGame.setShips(ships);
+            Main.currentGame.transmittSizeAndShips();
+
+            parent.openGameWindow();
+         }
+      });
+
+      // Alles in Wrapper einfügen
+      wrapperPanel.add(sizeRow);
+      wrapperPanel.add(shipsInputRow1);
+      wrapperPanel.add(shipsInputRow2);
+      wrapperPanel.add(startGameButton);
+
+      // Menu Button
+      JButton menu = new MenuButton();
+      menu.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent event) {
+            Main.currentGame.exit();
+            System.out.println(event.getActionCommand());
+            parent.showMenu();
+         }
+      });
+
+      // Alles einfügen
+      add(menu);
+      add(Box.createGlue());
+      add(wrapperPanel);
+      add(Box.createGlue());
 	}
-	
+
 	public void processNotification(String type, Object object) {
 		if (type.equals("ClientConnected")) {
-			start.setEnabled(true);
+			startGameButton.setEnabled(true);
 		}
 	}
-	
-	
-	
-	
-	
-	private class MenuButton extends JButton {
-		private static final long serialVersionUID = 2634914788445693027L;
-
-		MenuButton() {
-			super("Menü");
-			addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					System.out.println(event.getActionCommand());
-					parent.showMenu();
-				}
-			});
-		}
-	}
-	
 }
--- src/UserInterface/MenuPanels/SettingsPanel.java
+++ src/UserInterface/MenuPanels/SettingsPanel.java
@@ -3,21 +3,37 @@ package UserInterface.MenuPanels;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 
 import UserInterface.Menu;
+import UserInterface.UIComponents.DualRowPanel;
+import UserInterface.UIComponents.HeaderLabel;
+import UserInterface.UIComponents.InputButton;
+import UserInterface.UIComponents.InputPanel;
+import UserInterface.UIComponents.InputSpinner;
+import UserInterface.UIComponents.InputTextField;
+import UserInterface.UIComponents.MenuButton;
+import UserInterface.UIComponents.WrapperPanel;
+
+// TODO: Menu button: position top left
+// TODO: Prefill values
+// TODO: Give savebutton function
+// TODO: theme path input
 
 public class SettingsPanel extends JPanel {
 
-	
-	
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -7953362830551040945L;
-	
+
 	Menu parent;
 
 	public SettingsPanel(Menu parent) {
@@ -25,33 +41,86 @@ public class SettingsPanel extends JPanel {
 		setup();
 		fillWithContent();
 	}
-	
+
 	private void setup() {
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 	}
-	
-	private void fillWithContent() {
-		JButton menu = new MenuButton();
-		
-		add(menu);
-	}
-	
-	
-	
-	
-	
-	private class MenuButton extends JButton {
-		private static final long serialVersionUID = 2634914788445693027L;
 
-		MenuButton() {
-			super("Menü");
-			addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					System.out.println(event.getActionCommand());
-					parent.showMenu();
-				}
-			});
-		}
+	private void fillWithContent() {
+      JPanel wrapperPanel = new WrapperPanel();
+
+      JLabel colorHeader = new HeaderLabel("Farben");
+
+      JPanel colorsFirstRow = new DualRowPanel();
+      InputPanel backgroundInputPanel = new InputPanel("Hintergrund");
+      JTextField backgroundInput = new InputTextField();
+      backgroundInputPanel.add(backgroundInput);
+      InputPanel foregroundInputPanel = new InputPanel("Schrift");
+      JTextField foregroundInput = new InputTextField();
+      foregroundInputPanel.add(foregroundInput);
+      colorsFirstRow.add(backgroundInputPanel);
+      colorsFirstRow.add(foregroundInputPanel);
+
+      JPanel colorsSecondRow = new DualRowPanel();
+      InputPanel buttonBackgroundInputPanel = new InputPanel("Button - Hintergrund");
+      JTextField buttonBackgroundInput = new InputTextField();
+      buttonBackgroundInputPanel.add(buttonBackgroundInput);
+      InputPanel buttonForegroundInputPanel = new InputPanel("Button - Schrift");
+      JTextField buttonForegroundInput = new InputTextField();
+      buttonForegroundInputPanel.add(buttonForegroundInput);
+      colorsSecondRow.add(buttonBackgroundInputPanel);
+      colorsSecondRow.add(buttonForegroundInputPanel);
+
+      JPanel colorsThirdRow = new DualRowPanel();
+      InputPanel borderInputPanel = new InputPanel("Border");
+      JTextField borderInput = new InputTextField();
+      borderInputPanel.add(borderInput);
+      InputPanel errorInputPanel = new InputPanel("Error");
+      JTextField errorInput = new InputTextField();
+      errorInputPanel.add(errorInput);
+      colorsThirdRow.add(borderInputPanel);
+      colorsThirdRow.add(errorInputPanel);
+
+      JLabel borderHeader = new HeaderLabel("Borders");
+
+      JPanel bordersFirstRow = new DualRowPanel();
+      InputPanel borderWidthInputPanel = new InputPanel("Breite");
+      JSpinner borderWidthInput = new InputSpinner(new SpinnerNumberModel(2, 0, 4, 1));
+      borderWidthInputPanel.add(borderWidthInput);
+      InputPanel borderRadiusInputPanel = new InputPanel("Radius");
+      JSpinner borderRadiusInput = new InputSpinner(new SpinnerNumberModel(15, 0, 25, 1));
+      borderRadiusInputPanel.add(borderRadiusInput);
+      bordersFirstRow.add(borderWidthInputPanel);
+      bordersFirstRow.add(borderRadiusInputPanel);
+
+      JLabel themeHeader = new HeaderLabel("Theme");
+
+      // theme path input
+
+      JButton saveButton = new InputButton("Speichern");
+      // give savebutton function
+
+      wrapperPanel.add(colorHeader);
+      wrapperPanel.add(colorsFirstRow);
+      wrapperPanel.add(colorsSecondRow);
+      wrapperPanel.add(colorsThirdRow);
+      wrapperPanel.add(borderHeader);
+      wrapperPanel.add(bordersFirstRow);
+      wrapperPanel.add(themeHeader);
+      wrapperPanel.add(saveButton);
+
+      JButton menuButton = new MenuButton();
+      menuButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent event) {
+            System.out.println(event.getActionCommand());
+            parent.showMenu();
+         }
+      });
+
+		add(menuButton); // Position top left
+      menuButton.setLocation(20, 20);
+      add(Box.createGlue());
+      add(wrapperPanel);
+      add(Box.createGlue());
 	}
-	
 }
