b777aeea24e3df6ee4ccfe6f99cdcffc6a9376f6
Linus Grohmann <linus.grohmann@gmx.de>
2022-12-07 13:43:34 +0100

    AI enemy überlegungen

--- src/Schiffeversenken/AIPlayer.java
+++ src/Schiffeversenken/AIPlayer.java
@@ -12,3 +12,24 @@ public class AIPlayer extends Player {
 	}
 
 }
+
+
+
+/* Schiffe dürfen sich nicht berühren -> Felder neben Schiffen prio 0
+ * Schiffe sind mindestens 2 Felder lang und in einer Linie -> sobald 2 benachbarte Teile bekannt sind ist entweder:
+ * 			  an einem Ende answer 0 -> an anderem Ende prio max
+ * 				  für answer 1: offenes Ende prio max
+ * 				  für answer 2: Schiff zerstört
+ * 		oder: an einem Ende answer 1 -> auf dieses Ende prio max
+ * 				  für answer 0: auf anderes Ende prio max
+ * 					für answer 1: offenes Ende prio max
+ * 					für answer 2: Schiff zerstört
+ * 				  für answer 2: Schiff zerstört
+ * Schiffanzahl pro Länge bekannt -> Lücken die kleiner als größtes noch vorhandenes Schiff sind -> Felder prio 0
+ * 
+ * Bei answer 0: direkte Nachbarfelder: prio norm -10
+ * 				 diagonale Nachbarfelder: prio norm -5
+ * 
+ * Schuss immer auf höchste prio
+ * 
+ */
