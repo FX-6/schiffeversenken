16d871491dddc7a71aaf4109c63a726b0e6e1d35
FX-6 <felixschwinger@web.de>
2022-12-11 22:00:27 +0100

    Added Label for default Text

---
+++ src/UserInterface/UIComponents/TextLabel.java
@@ -0,0 +1,19 @@
+package UserInterface.UIComponents;
+
+// TODO get height with content to adjust itemHeight in wrapperPanel (Felix)
+
+public class TextLabel extends UILabel {
+   public TextLabel() {
+      super();
+      setup();
+   }
+
+   public TextLabel(String text) {
+      super(text);
+      setup();
+   }
+
+   public void setup() {
+      // get height and set preffered height
+   }
+}
