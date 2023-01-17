aa06c84891dbb5f2eceabd6021fe2733fc190cf8
FX-6 <felixschwinger@web.de>
2023-01-17 15:10:36 +0100

    More JavaDoc and Formatting

--- src/UserInterface/UIComponents/GameMenuPanel.java
+++ src/UserInterface/UIComponents/GameMenuPanel.java
@@ -2,18 +2,38 @@ package UserInterface.UIComponents;
 
 import java.awt.Dimension;
 
+/**
+ * Wird als Wrapper für das Menü während der Runde genutzt.
+ */
 public class GameMenuPanel extends WrapperPanel {
-   private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-   public GameMenuPanel() {
-      super();
+	/**
+	 * Erstellt den Wrapper für das ingame Menü.
+	 */
+	public GameMenuPanel() {
+		super();
 
-      this.itemWidth = itemSmallWidth;
-      this.itemHeigth = itemSmallHeigth;
-      this.setSize(2 * borderWidth + 2 * padding + this.itemWidth, this.getWidth());
-   }
+		this.itemWidth = itemSmallWidth;
+		this.itemHeigth = itemSmallHeigth;
+		this.setSize(2 * borderWidth + 2 * padding + this.itemWidth, this.getWidth());
+	}
 
-   public Dimension smallDimension() { return new Dimension(this.itemWidth / 2 - padding / 2, this.itemHeigth); }
+	/**
+	 * Liefert die Werte für eine kleine UI-Komponente.
+	 *
+	 * @return Die {@link Dimension} einer kleinen Komponente
+	 */
+	public Dimension smallDimension() {
+		return new Dimension(this.itemWidth / 2 - padding / 2, this.itemHeigth);
+	}
 
-   public Dimension largeDimension() { return new Dimension(this.itemWidth, this.itemHeigth); }
+	/**
+	 * Liefert die Werte für eine große UI-Komponente.
+	 *
+	 * @return Die {@link Dimension} einer großen Komponente
+	 */
+	public Dimension largeDimension() {
+		return new Dimension(this.itemWidth, this.itemHeigth);
+	}
 }
