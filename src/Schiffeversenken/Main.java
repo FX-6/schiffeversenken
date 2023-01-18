5bfa55bc01fe2e15484f0a391bb47c52a623489d
Patrick Flögel <p.floegel@gmx.net>
2023-01-18 16:01:54 +0100

    Fertigstellung aller Kommentare für Systemdokumentation + Entfernung des Command-Line Programms, das bereits auskommentiert war

--- src/Schiffeversenken/Main.java
+++ src/Schiffeversenken/Main.java
@@ -4,119 +4,28 @@ import UserInterface.Menu;
 
 public class Main {
 
-	public static Game currentGame = null;
-	public static String hostAddress = null;
+	public static Game currentGame = null;			// Speichert das aktuelle Spiel ("null", falls keins gespielt oder vorbereitet wird) 
+	public static String hostAddress = null;		// Speicherung der IP-Adresse des Servers, falls ein Spiel über das Netzwerk gespielt werden soll, in welchem diese Instanz des Spiels als Client agieren wird
 
-	public static Menu menuWindow = null;
+	public static Menu menuWindow = null;			// Speicherung des Menü-Fensters
 
 	public static void main(String[] args) {
 
-      // creates game folder and settings file
+		// Erstellt Datei mit Einstellungen und lädt benötigte Assets herunter
+		// HINWEIS: Beim ersten Start des Spiels wird folglich eine aktive Verbindung zum Internet benötigt!
 		SettingsHandler.initSettings();
 
+		// Erzeugt das Menü-Fenster, über welches alles gesteuert wird
 		menuWindow = new Menu();
-
-		
-		
-		
-
-//		createGame(10, GameType.NETWORK_SERVER);
-//
-//		int[] ships = new int[4];
-//		Arrays.fill(ships, 0);
-//		ships[2] = 1;
-//		currentGame.setShips(ships);
-
-		/*
-		System.out.println("1.\tcreate [server | client <ipAddress>] pitchSize \t//if Client then just any number for pitchSize");
-		System.out.println("2.\tships <size 2> <size 3> <size 4> <size 5> \t//if Server");
-		System.out.println("3.\tstart \t//if Server");
-		System.out.println("4.\t[place <length> <orientation> | get] <x> <y>");
-		System.out.println("5.\tready");
-		System.out.println("6.\tshoot <x> <y>");
-		System.out.println("7.\tquit");
-
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		String input = scanner.next();
-
-		// Create a new Game
-		while(!input.equals("create")) {
-			input = scanner.next();
-		}
-
-		while(!input.equals("server") && !input.equals("client")) {
-			input = scanner.next();
-		}
-
-		if (input.equals("server")) {
-			createGame(scanner.nextInt(), GameType.NETWORK_SERVER);
-
-			// Set Ships if server
-			while(!input.equals("ships") && hostAddress == null) {
-				input = scanner.next();
-			}
-
-			int[] ships = new int[4];
-			ships[0] = scanner.nextInt();
-			ships[1] = scanner.nextInt();
-			ships[2] = scanner.nextInt();
-			ships[3] = scanner.nextInt();
-
-			currentGame.setShips(ships);
-
-			while(!input.equals("start")) {
-				input = scanner.next();
-			}
-
-			currentGame.transmittSizeAndShips();
-		}
-		else if (input.equals("client")) {
-			hostAddress = scanner.next();
-			createGame(scanner.nextInt(), GameType.NETWORK_CLIENT);
-		}
-
-
-		while (!input.equals("ready")) {
-			// place <length> <orientation> <x> <y>	--> Plaziert Schiff
-			if (input.equals("place")) {
-				System.out.println(currentGame.getPlayer1().placeShipAt(new Ship(scanner.nextInt(), scanner.nextInt()), new Point(scanner.nextInt(), scanner.nextInt())));
-			}
-
-			// get <x> <y> --> Gibt Schiff zurueck
-			if (input.equals("get")) {
-				System.out.println(currentGame.getPlayer1().getShipAt(new Point(scanner.nextInt(), scanner.nextInt())));
-			}
-
-			input = scanner.next();
-		}
-
-		currentGame.transmittReady();
-
-
-		while (!input.equals("quit") && currentGame != null) {
-
-			// shoot <x> <y>	--> Schiesst auf gegnerisches Schiff
-			if (input.equals("shoot")) {
-				int result = currentGame.getPlayer1().shoot(new Point(scanner.nextInt(), scanner.nextInt()));
-				if (result == 0) currentGame.getPlayer1().otherPlayer.pass();
-			}
-
-			// get <x> <y> --> Gibt Schiff zurueck
-			if (input.equals("get")) {
-				System.out.println(currentGame.getPlayer1().getShipAt(new Point(scanner.nextInt(), scanner.nextInt())));
-			}
-
-			input = scanner.next();
-		}
-
-		currentGame.exit();
-		*/
-
 	}
 
 
-
+	/**
+	 * Kreiert eine neue Instanz eines Spiels mit allen benötigten Klassen.
+	 * 
+	 * @param pitchSize Seitenlänge in Feldern des Spielfelds für das Spiel.
+	 * @param type Zusammensetzung der beiden Spieler, die gegeneinander spielen sollen.
+	 */
 	public static void createGame(int pitchSize, GameType type) {
 		Player player1 = null;
 		Player player2 = null;
