c5eb1b945421d716fd2f0cc7b32e2d914e9b05ac
Patrick Flögel <p.floegel@gmx.net>
2023-01-18 16:02:14 +0100

    Fertigstellung aller Kommentare für Systemdokumentation

--- src/Schiffeversenken/GameType.java
+++ src/Schiffeversenken/GameType.java
@@ -1,9 +1,31 @@
 package Schiffeversenken;
 
+/**
+ * Zusammensetzung der beiden Spieler, die gegeneinander spielen sollen.
+ */
 public enum GameType {
+	/**
+	 * Multiplayer über das Netzwerk als menschlicher Spieler. Diese Instanz des Spiels agiert als Server.
+	 */
 	NETWORK_SERVER,
+	
+	/**
+	 * Multiplayer über das Netzwerk als menschlicher Spieler. Diese Instanz des Spiels agiert als Client.
+	 */
 	NETWORK_CLIENT,
+	
+	/**
+	 * Singleplayer gegen den Computer.
+	 */
 	AI,
+	
+	/**
+	 * Computer spielt über das Netzwerk. Diese Instanz des Spiels agiert als Server.
+	 */
 	NETWORK_AI_SERVER,
+	
+	/**
+	 * Computer spielt über das Netzwerk. Diese Instanz des Spiels agiert als Client.
+	 */
 	NETWORK_AI_CLIENT
 }
