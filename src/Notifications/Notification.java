8477ceea6ed3db2b28ec3ed5d8c73e657c468a8f
Patrick Flögel <p.floegel@gmx.net>
2023-01-19 16:18:24 +0100

    Fertigstellung aller Kommentare für Systemdokumentation

--- src/Notifications/Notification.java
+++ src/Notifications/Notification.java
@@ -1,11 +1,18 @@
 package Notifications;
 
-/*
- * Dieses Interface muss implementiert werden, um eine Nachricht über das NotificationCenter zu empfangen. Beim versenden einer Nachricht
- * wird die Funktion processNotification aufgerufen. Es muss dann ueberprueft werden, ob es sich bei "type" auch tatsaechlich um das tatsaechlich
- * abbonierte Event handelt (nur relevant, wenn mehrere Events abbiert wurden).
+/**
+ * Dieses Interface muss implementiert werden, um eine Rundnachricht über das NotificationCenter zu empfangen. Beim versenden einer Nachricht
+ * wird die Funktion processNotification aufgerufen. Es muss dann ueberprueft werden, ob es sich bei "type" auch tatsaechlich um das
+ * abbonierte Event handelt (nur relevant, wenn mehrere Events abboniert wurden).
  */
 
 public interface Notification {
+	
+	/**
+	 * Verarbeitet eine Rundnachricht / ein Event, die durch {@link NotificationCenter#sendNotification(String, Object)} gesendet wurde.
+	 * 
+	 * @param type Name der Rundnachricht / des Events.
+	 * @param object Eventueller Anhang (kann auch "null" sein).
+	 */
 	public void processNotification(String type, Object object);
 }
