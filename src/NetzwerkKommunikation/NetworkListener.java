e2cacb56b926c45d07e3076dd1ac9136b2d7cb94
Patrick Flögel <p.floegel@gmx.net>
2023-01-19 16:57:14 +0100

    Fertigstellung aller Kommentare für Systemdokumentation

--- src/NetzwerkKommunikation/NetworkListener.java
+++ src/NetzwerkKommunikation/NetworkListener.java
@@ -8,8 +8,8 @@ import Schiffeversenken.GameExitStatus;
 import Schiffeversenken.Main;
 
 
-/*
- * Ein Objekt dieser Klasse wartet kontinuierlich auf eine Nachricht aus dem Netzwerk (asynchron zum Rest des Programms).
+/**
+ * Ein Objekt dieser Klasse wartet kontinuierlich auf eine Nachricht aus dem Netzwerk (asynchron zum Rest des Programms in einem anderen Thread).
  * Um das PingPong-Prinzip einzuhalten muss also die UI das Senden weiterer Nachrichten unterbinden!
  * 
  * Wenn eine Nachricht eingetroffen ist, wird das Event "NewNetworkMessage" ausgelöst und alle Objekte, die dieses Event abboniert 
@@ -18,26 +18,41 @@ import Schiffeversenken.Main;
 
 public class NetworkListener extends Thread {
 	
+	/**
+	 * InputStream der Verbindung.
+	 */
 	private BufferedReader in;
 		
+	
+	
+	/**
+	 * Erzeugt einen neuen Thread mit einem Listener.
+	 * 
+	 * @param in InputStream der Verbindung.
+	 */
 	public NetworkListener(BufferedReader in) {
-		super("Listener");
+		super("Listener");							// Thread mit Namen "Listener" erzeugen
 		this.in = in;
 	}
 	
+	
+	
 	public void run() {
+		// Kontinuierlich auf eine Nachricht warten
 		while(true) {
 			String message = "";
+			
 			try {
 				message = "" + in.readLine();
 			} catch (IOException e) {
+				// Verbindung wurde beendet
 				if (Main.currentGame != null) Main.currentGame.exit(Main.currentGame.getPlayer2(), GameExitStatus.CONNECTION_CLOSED);
-				//e.printStackTrace();
 				break;
 			}
 			
 			NotificationCenter.sendNotification("NewNetworkMessage", message);			// Loest Event "NewNetworkMessageEvent" aus (Neue Nachricht über das Netzwerk empfangen)
 			
+			// Verbindung wurde beendet
 			if (message.equals("null")) {
 				break;
 			}
