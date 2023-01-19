ca096b1bb65c75e5dc07c70c035f7aed7e45a0c8
Patrick Flögel <p.floegel@gmx.net>
2023-01-19 16:57:30 +0100

    Fertigstellung aller Kommentare für Systemdokumentation

--- src/Notifications/NotificationCenter.java
+++ src/Notifications/NotificationCenter.java
@@ -5,7 +5,7 @@ import java.util.ConcurrentModificationException;
 import java.util.HashMap;
 import java.util.List;
 
-/*
+/**
  * Diese Klasse bietet eine Kommunikationsebene für Objekte, die sich gegenseitig nicht referenzieren.
  * Um diese zu nutzen, muss die empfangende Klasse das Interface Notification implementieren und mit NotificationCenter.addObserver()
  * als Empfänger registriert werden.
@@ -15,28 +15,45 @@ import java.util.List;
 
 public final class NotificationCenter {
 
+	/**
+	 * Speichert alle Abbonenten einer Nachricht / eines Events in einer Liste unter dem Namen der Nachricht / des Events.
+	 */
 	private static HashMap<String, ArrayList<Notification>> listeners = new HashMap<String, ArrayList<Notification>>();
 	
-	// Sendet eine Nachricht an alle Abbonenten des Events "type"
+	/**
+	 * Sendet eine Nachricht an alle Abbonenten der Nachricht / des Events "type"
+	 * 
+	 * @param type Name der Nachricht / des Events.
+	 * @param object Eventueller Anhang (kann auch "null" sein).
+	 */
 	public static void sendNotification(String type, Object object) {
 		List<Notification> list = listeners.get(type);
 		if (list == null) return;
 		for (Notification listener : list) {
 			try {
-				listener.processNotification(type, object);
+				listener.processNotification(type, object);				// Ruft die Methode zur Abarbeitung der Nachricht / des Events bei allen Abbonenten auf
 			} catch (ConcurrentModificationException e) {
-				e.printStackTrace();
+				// Es kann vorkommen, dass der Inhalt von "list" durch  den Aufruf von removeAllObservers(Object object) in einem anderen Thread verändert wird, während obige Schleife gerade durch die Liste iteriert.
+				// Dies ist für diese Anwendung nicht weiter schlimm. Das Objekt wird zwar nicht aus der Liste enftfernt, der Sinn des Entfernen ist allerdings auch nur, dass es nicht mehr referenziert wird, sodass es von
+				// der GarbageCollection freigegeben wird.
 			}
 		}
 	}
 	
-	// Abboniert das Event "type" für das Object "notification"
+	/**
+	 * Abboniert eine Nachricht / ein Event.
+	 * 
+	 * @param type Name der Nachricht / des Events, welche(s) abboniert werden soll.
+	 * @param notification Objekt, welches die Nachricht / das Event abboniert.
+	 */
 	public static void addObserver(String type, Notification notification) {
+		// Wenn es der erste Abbonent für diese Nachricht / dieses Event ist -> es existiert noch keine Liste der Abbonenten
 		if (listeners.get(type) == null) {
 			ArrayList<Notification> list = new ArrayList<Notification>();
 			list.add(notification);
 			listeners.put(type, list);
 		}
+		// Andernfalls
 		else {
 			ArrayList<Notification> list = listeners.get(type);
 			list.add(notification);
@@ -44,6 +61,12 @@ public final class NotificationCenter {
 		}
 	}
 	
+	
+	/**
+	 * Deabboniert alle Nachrichten / Events, die übergebenes Objekt abboniert hat.
+	 * 
+	 * @param object Objekt, dessen Abonements entfernt werden sollen.
+	 */
 	public static void removeAllObservers(Object object) {
 		for (ArrayList<Notification> list : listeners.values()) {
 			list.remove(object);
